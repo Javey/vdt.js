1a24510350d25955ce0fdc71f66683f09d29d14b
Javey <jiawei23716@sina.com>
2015-04-22 23:06:05 +0800

    todo demo

--- test/app.js
+++ test/app.js
@@ -1,18 +1,37 @@
-var templateDom = document.getElementById('todo_template')
+var templateDom = document.getElementById('todo_template'),
     templateStr = templateDom.innerHTML,
     templateFn = Vdt(templateStr);
 
-var list = [
-    {
-        title: 'aaa',
-        value: '111',
-        completed: true
-    },
-    {
-        title: 'bbb',
-        value: '222',
-        completed: false
-    }
-];
+var list = [];
 
-templateDom.parentNode.insertAfter(templateFn({list: list}), templateDom);
+var dom = templateFn({list: list});
+templateDom.parentNode.insertBefore(dom, templateDom);
+
+var input = document.querySelector('.new-todo');
+input.addEventListener('change', function(e) {
+    list.push({
+        title: e.target.value,
+        completed: false
+    });
+    e.target.value = '';
+    update();
+});
+
+dom.addEventListener('click', function(e) {
+    if (e.target.className === 'destroy') {
+        var index = e.target.parentNode.parentNode.index;
+        list.splice(index, 1);
+        update();
+    }
+});
+
+document.querySelector('.toggle-all').addEventListener('click', function(e) {
+    list.forEach(function(item) {
+        item.completed = e.target.checked;
+    });
+    update();
+});
+
+function update() {
+    templateFn.update({list: list});
+}
