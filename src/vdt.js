dced3cfa77f53a0d88f9a8c0c048f888a8afede8
Javey <jiawei23716@sina.com>
2015-04-23 16:09:19 +0800

    move build to dist

--- src/vdt.js
+++ src/vdt.js
@@ -9,16 +9,17 @@ var Vdt = function(source) {
     hscript = 'var h = Vdt.virtualDom.h;\nwith(obj) {' + hscript + '};';
     var templateFn = new Function('obj', hscript);
 
-    var tree, node;
+    var tree, node, self;
 
-    var ret = function(data) {
-        tree = templateFn(data);
+    var ret = function(data, thisArg) {
+        self = thisArg;
+        tree = templateFn.call(thisArg, data);
         node = virtualDom.create(tree);
         return node;
     };
 
     ret.update = function(data) {
-        var newTree = templateFn(data),
+        var newTree = templateFn.call(self, data),
             patches = virtualDom.diff(tree, newTree);
         node = virtualDom.patch(node, patches);
         tree = newTree;
--- test/app.js
+++ test/app.js
@@ -1,10 +1,17 @@
 (function($, Vdt) {
     'use strict';
 
+    var ENTER_KEY = 13,
+        ESCAPE_KEY = 27;
+
     var Model = {
         list: [
             {title: 'test', completed: false}
         ],
+
+        filterType: 'all',
+
+        editingIndex: -1,
 
         create: function(title) {
             this.list.push({
@@ -30,6 +37,22 @@
             this.trigger('change');
         },
 
+        set: function(key, value) {
+            this[key] = value;
+            this.trigger('change');
+        },
+
+        filter: function(item) {
+            switch (this.filterType) {
+                case 'active':
+                    return !item.completed;
+                case 'completed':
+                    return item.completed;
+                default:
+                    return true;
+            }
+        },
+
         _events: {},
 
         on: function(event, callback) {
@@ -50,37 +73,48 @@
     var View = {
         init: function() {
             var $template = $('#todo_template');
-            this.template = Vdt($template.html());
-            this.$dom = $(this.template(Model));
-            $template.after(this.$dom);
+            this.vdt = Vdt($template.html());
+
+            this._setFilterType();
+
+            this.$el = $(this.vdt(Model));
+            $template.after(this.$el);
 
             this._bindEvent();
         },
 
         _bindEvent: function() {
-            this.$dom
+            this.$el
                 .on('change', '.new-todo', this._add.bind(this))
                 .on('click', '.destroy', this._destroy.bind(this))
                 .on('click', '.toggle-all', this._toggleAll.bind(this))
-                .on('click', '.toggle', this._toggle.bind(this));
+                .on('click', '.toggle', this._toggle.bind(this))
+                .on('click', '.clear-completed', this._clearCompleted.bind(this))
+                .on('dblclick', '.todo-list label', this._edit.bind(this))
+                .on('keyup', '.edit', this._editKeyup.bind(this))
+                .on('blur', '.edit', this._editDone.bind(this));
+
+            $(window).on('hashchange', this._setFilterType.bind(this));
 
             Model.on('change', this._update.bind(this));
         },
 
         _destroy: function(e) {
-            var index = $(e.target).closest('li')[0].index;
-            Model.remove(index);
+            Model.remove(this._index($(e.target)));
         },
 
         _update: function() {
-            this.template.update(Model);
+            this.vdt.update(Model);
         },
 
         _add: function(e) {
             var $input = $(e.target),
-                value = $input.val();
+                value = $input.val().trim();
+
+            if (value) {
+                Model.create(value);
+            }
             $input.val('');
-            Model.create(value);
         },
 
         _toggleAll: function(e) {
@@ -88,9 +122,56 @@
         },
 
         _toggle: function(e) {
+            var $target = $(e.target);
+
+            Model.update('completed', $target.prop('checked'), this._index($target));
+        },
+
+        _setFilterType: function() {
+            var hash = location.hash.substring(2);
+            if (!~['all', 'active', 'completed'].indexOf(hash)) {
+                hash = 'all'
+            }
+            Model.set('filterType', hash);
+        },
+
+        _clearCompleted: function() {
+            var list = Model.list.filter(function(item) {
+                return !item.completed;
+            });
+            Model.set('list', list);
+        },
+
+        _edit: function(e) {
+            var $target= $(e.target);
+
+            Model.set('editingIndex', this._index($target));
+            $target.closest('li').find('.edit').focus();
+        },
+
+        _editKeyup: function(e) {
+            var $target = $(e.target);
+
+            if (e.which === ENTER_KEY) {
+                $target.blur();
+            } else if (e.which === ESCAPE_KEY) {
+                $target.val(Model.list[this._index($target)].title);
+                $target.blur();
+            }
+        },
+
+        _editDone: function(e) {
             var $target = $(e.target),
-                index = $target.closest('li')[0].index;
-            Model.update('completed', $target.prop('checked'), index);
+                value = $target.val().trim();
+
+            if (value) {
+                Model.update('title', value, this._index($target));
+            }
+            Model.set('editingIndex', -1);
+        },
+
+        _index: function($target) {
+            return $target.closest('li')[0].index
         }
     };
 
