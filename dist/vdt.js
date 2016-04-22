08783e207ac86e262530e26af8730df4458c0263
Javey <jiawei23716@sina.com>
2016-04-22 15:52:54 +0800

    hot fixed

--- dist/vdt.js
+++ dist/vdt.js
@@ -2162,7 +2162,7 @@ function updateWidget(a, b) {
     if (isWidget(a) && isWidget(b)) {
         if ("name" in a && "name" in b) {
             return a.id === b.id
-        } else if (!a.constructor instanceof a.constructor || !b.constructor instanceof b.constructor) {
+        } else if (!(a.constructor instanceof a.constructor) || !(b.constructor instanceof b.constructor)) {
             return a.constructor === b.constructor
         } else {
             return a.init === b.init
