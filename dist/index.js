98162f449506002bc6c2eb3bb5851974b9aef925
Javey <jiawei23716@sina.com>
2017-10-23 15:41:04 +0800

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -1505,9 +1505,7 @@ function createVNode(tag, props, children, className, key, ref) {
 
     if (type & Types.ComponentClass) {
         if (!isNullOrUndefined(children)) {
-            if (props === EMPTY_OBJ) {
-                props = {};
-            }
+            if (props === EMPTY_OBJ) props = {};
             props.children = normalizeChildren(children);
         } else if (!isNullOrUndefined(props.children)) {
             props.children = normalizeChildren(props.children);
@@ -1826,7 +1824,7 @@ function processInput(vNode, dom, nextProps) {
     var defaultValue = nextProps.defaultValue;
     var multiple = nextProps.multiple;
     var hasValue = nextProps.hasOwnProperty('value');
-    var value = hasValue ? nextProps.value || '' : undefined;
+    var value = hasValue ? nextProps.value : undefined;
 
     if (multiple && multiple !== dom.multiple) {
         dom.multiple = multiple;
@@ -2698,7 +2696,7 @@ function patchProp(prop, lastValue, nextValue, dom, isFormElement, isSVG) {
             if (dom[prop] !== value || browser.isIE8) {
                 dom[prop] = value;
             }
-            // add a private property _value for select an object
+            // add a private property _value for selecting an non-string value 
             if (prop === 'value') {
                 dom._value = value;
             }
